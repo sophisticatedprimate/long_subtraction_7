a3f6c6f9d7dd9f31d70273251b66a9d6d2d29580
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-19 19:15:48 -0500

    Add files via upload

---
+++ long_subtraction_7.py
@@ -0,0 +1,111 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Refer to long_subtraction_1.py for further information on subtracting values in python:
+https://github.com/sophisticatedprimate/long_subtraction_1/blob/main/long_subtraction_1.py
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed
+    2. Navigate to a directory containing this file (long_subtraction_7.py)
+    3. Run the program with the following command: python3 long_subtraction_7.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1 
+difference = 2077792 - 1734164  
+print("{0:<8} - {1:<8} = {2:<8}".format(2077792,1734164,difference)) 
+
+# Problem 2 
+difference = 7636925 - 1751432  
+print("{0:<8} - {1:<8} = {2:<8}".format(7636925,1751432,difference)) 
+
+# Problem 3 
+difference = 11331784 - 548820  
+print("{0:<8} - {1:<8} = {2:<8}".format(11331784,548820,difference)) 
+
+# Problem 4 
+difference = 15943118 - 2081659  
+print("{0:<8} - {1:<8} = {2:<8}".format(15943118,2081659,difference)) 
+
+# Problem 5 
+difference = 16836727 - 324227  
+print("{0:<8} - {1:<8} = {2:<8}".format(16836727,324227,difference)) 
+
+# Problem 6 
+difference = 4543114 - 1673593  
+print("{0:<8} - {1:<8} = {2:<8}".format(4543114,1673593,difference)) 
+
+# Problem 7 
+difference = 2883970 - 1673433  
+print("{0:<8} - {1:<8} = {2:<8}".format(2883970,1673433,difference)) 
+
+# Problem 8 
+difference = 187154 - 95481  
+print("{0:<8} - {1:<8} = {2:<8}".format(187154,95481,difference)) 
+
+# Problem 9 
+difference = 15398049 - 754082  
+print("{0:<8} - {1:<8} = {2:<8}".format(15398049,754082,difference)) 
+
+# Problem 10
+difference = 1096690 - 510317  
+print("{0:<8} - {1:<8} = {2:<8}".format(1096690,510317,difference)) 
+
+# Problem 11
+difference = 18040358 - 1509557  
+print("{0:<8} - {1:<8} = {2:<8}".format(18040358,1509557,difference)) 
+
+# Problem 12
+difference = 11173456 - 1463127  
+print("{0:<8} - {1:<8} = {2:<8}".format(11173456,1463127,difference)) 
+
+# Problem 13
+difference = 5868827 - 1961253  
+print("{0:<8} - {1:<8} = {2:<8}".format(5868827,1961253,difference)) 
+
+# Problem 14
+difference = 8259705 - 1849723  
+print("{0:<8} - {1:<8} = {2:<8}".format(8259705,1849723,difference)) 
+
+# Problem 15
+difference = 19438142 - 932998  
+print("{0:<8} - {1:<8} = {2:<8}".format(19438142,932998,difference)) 
+
+# Problem 16
+difference = 18891296 - 1402080  
+print("{0:<8} - {1:<8} = {2:<8}".format(18891296,1402080,difference)) 
+
+# Problem 17
+difference = 3505911 - 1145099  
+print("{0:<8} - {1:<8} = {2:<8}".format(3505911,1145099,difference)) 
+
+# Problem 18
+difference = 2090461 - 807931  
+print("{0:<8} - {1:<8} = {2:<8}".format(2090461,807931,difference)) 
+
+# Problem 19
+difference = 18403765 - 417463  
+print("{0:<8} - {1:<8} = {2:<8}".format(18403765,417463,difference)) 
+
+# Problem 20
+difference = 9579024 - 1971113  
+print("{0:<8} - {1:<8} = {2:<8}".format(9579024,1971113,difference)) 
+
+# Problem 21
+difference = 12527585 - 996979  
+print("{0:<8} - {1:<8} = {2:<8}".format(12527585,996979,difference)) 
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_subtraction_7/issues
+"""
